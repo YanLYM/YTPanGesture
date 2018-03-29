fd45dea72c407972bab9c87c1ed868d9fbb5cd69
Max <yanzhihao@edspay.com>
2018-03-29 13:23:23 +0800

    扫描Vc

---
+++ YTPanGesture/YTPanGesture/Scan/YTScanViewController.h
@@ -0,0 +1,13 @@
+//
+//  YTScanViewController.h
+//  YTPanGesture
+//
+//  Created by Max on 2018/3/29.
+//  Copyright © 2018年 Max. All rights reserved.
+//
+
+#import <LBXScan/LBXScanViewController.h>
+
+@interface YTScanViewController : LBXScanViewController
+
+@end
